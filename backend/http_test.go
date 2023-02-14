b150e68b0d6a8ce289081c7cee47f6b3d4d61440
probe-z <824002903@qq.com>
2023-02-14 21:15:44 +0800

    testcase

--- backend/http_test.go
+++ backend/http_test.go
@@ -12,32 +12,33 @@ import (
 )
 
 func Test(t *testing.T) {
+	// test server
+	go func() {
+		svrConf := httpserver.ServerConfig{
+			Addr:           "127.0.0.1:8080",
+			RestartTimeout: 3,
+		}
+		srv := httpserver.NewServer(&svrConf)
+		srv.HandleFunc("/hi", func(r httpserver.RequestContext) {
+			log.Printf("hi")
+			r.SetJsonResponse(&httpserver.JsonResponse{
+				Data: &struct{ Name string }{
+					Name: "dddd",
+				},
+			})
+		})
+		srv.Run()
+	}()
+
+	// test client
+	time.Sleep(time.Duration(1) * time.Second)
 	httpClientConf := &httpclient.HttpTransportConf{}
 	defaults.Set(httpClientConf)
 	httpclient.Configure(httpClientConf)
-
-	r := &httpclient.RequestContext{
+	(&httpclient.RequestContext{
 		Method:  "GET",
 		Timeout: time.Duration(2) * time.Second,
-		Url:     "http://www.baidu.com",
-	}
-	r.Do(context.Background())
-	svrConf := httpserver.ServerConfig{
-		Addr:           "127.0.0.1:8080",
-		RestartTimeout: 3,
-	}
-	go func() {
-		time.Sleep(time.Duration(5) * time.Second)
-		syscall.Kill(syscall.Getpid(), syscall.SIGUSR2)
-	}()
-	srv := httpserver.NewServer(&svrConf)
-	srv.HandleFunc("/hi", func(r httpserver.RequestContext) {
-		log.Printf("hi")
-		r.SetJsonResponse(&httpserver.JsonResponse{
-			Data: &struct{Name string}{
-				Name: "dddd",
-			},
-		})
-	})
-	srv.Run()
+		Url:     "http://127.0.0.1:8080/hi",
+	}).Do(context.Background())
+	syscall.Kill(syscall.Getpid(), syscall.SIGUSR2)
 }
